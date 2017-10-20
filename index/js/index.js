3d2bd25ef3e0dc74bfbd185f07f56a5daa0b9f71
Damian <damiansantae@gmail.com>
2017-10-20 17:35:56 +0100

    Probando js

---
+++ index/js/index.js
@@ -0,0 +1,9 @@
+var a = document.doctype.name;
+
+var b = document.inputEncoding;
+
+
+//Listener a boton de login
+
+var loginbtn = document.getElementById('btn-login');
+loginbtn.addEventListener('click', alert('Hiciste click en boton de login'));
