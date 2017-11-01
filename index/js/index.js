705bd758a7ca1ac6b92ae215b9a47d7eaf07b92e
Damian <damiansantae@gmail.com>
2017-11-01 10:06:10 +0000

    implementando validacion de formularios en index

--- index/js/index.js
+++ index/js/index.js
@@ -4,6 +4,28 @@ var b = document.inputEncoding;
 
 
 //Listener a boton de login
+function checkValidation(){
 
-var loginbtn = document.getElementById('btn-login');
-loginbtn.addEventListener('click', alert('Hiciste click en boton de login'));
+
+    document.getElementById('password');
+
+    //Validamos que el nombre de usuario es un nombre entre 2-20 caracteres
+    var username = document.getElementById('username');
+    if(!username.validity.patternMismatch){
+        username.setCustomValidity('El nombre de usuario debe de tener entre 2-20 caracteres alfanuméricos')
+    }
+
+
+    //comprobamos formato de email
+
+    var email = document.getElementById('email');
+    if (!email.validity.typeMismatch){
+        email.setCustomValidity('Use un formato adecuado de email (usuario@empresa.dominio')
+    }
+    //Comprobamos que las contraseñas se corresponden
+    var pwd1 = document.getElementById('password');
+    var pwd2 = document.getElementById('passwordr');
+    if(pwd1.value !== pwd2.value){
+        pwd1.setCustomValidity('Las contraseñas deben ser iguales');
+    }
+}
