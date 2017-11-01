0de04b595e24e4f8efd61c68fad2a40058081314
Damian <damiansantae@gmail.com>
2017-11-01 15:14:46 +0000

    implementando validacion de formularios en index

--- index/js/index.js
+++ index/js/index.js
@@ -1,31 +1,48 @@
 var a = document.doctype.name;
-
 var b = document.inputEncoding;
 
 
-//Listener a boton de login
+
 function checkValidation(){
-
-
-    document.getElementById('password');
 
     //Validamos que el nombre de usuario es un nombre entre 2-20 caracteres
     var username = document.getElementById('username');
-    if(!username.validity.patternMismatch){
+    if(username.validity.patternMismatch === true){
         username.setCustomValidity('El nombre de usuario debe de tener entre 2-20 caracteres alfanuméricos')
     }
 
-
-    //comprobamos formato de email
-
+   /* //comprobamos formato de email
     var email = document.getElementById('email');
-    if (!email.validity.typeMismatch){
+    if (email.validity.typeMismatch){
         email.setCustomValidity('Use un formato adecuado de email (usuario@empresa.dominio')
-    }
-    //Comprobamos que las contraseñas se corresponden
+    }*/
+
+
+
+    //Comprobamos que la contraseña debe tener una mayuscula, una minuscula, un numero y minimo 8 caracteres
     var pwd1 = document.getElementById('password');
-    var pwd2 = document.getElementById('passwordr');
-    if(pwd1.value !== pwd2.value){
-        pwd1.setCustomValidity('Las contraseñas deben ser iguales');
+
+   if (pwd1.validity.patternMismatch === true){
+        pwd1.setCustomValidity('la contraseña debe tener una mayúscula, una minúscula, un número y minimo 8 caracteres')
+    } else{
+        //Comprobamos que las contraseñas se corresponden
+        var pwd2 = document.getElementById('passwordr');
+        if(pwd1.value != pwd2.value){
+            pwd1.setCustomValidity('Las contraseñas deben ser iguales');
     }
+    }
+
+}
+
+function checkLogin() {
+   var dummyUser = document.getElementById('user-login');
+   var dummyPwd = document.getElementById('password-login');
+
+
+    window.location.href = "../../MainFrame/mainFrame.html";
+
+
+   /*if(dummyUser.value == 'damian' && dummyPwd.value == 'Password1'){
+   }*/
+
 }
