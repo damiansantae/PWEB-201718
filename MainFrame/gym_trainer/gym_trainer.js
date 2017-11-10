c67010b4dbdbc13b0eeb236440bd7b10ebb09053
Damian <damiansantae@gmail.com>
2017-11-10 11:03:08 +0000

    creando funcionalidad add nuevo dia de rutina

--- MainFrame/gym_trainer/gym_trainer.js
+++ MainFrame/gym_trainer/gym_trainer.js
@@ -1,18 +1,29 @@
 
 
 function searchExercise() {
-    var input, filter, list, exercises, h1, i;
+    var input, filter, list, exercises, p, i;
     input = document.getElementById("searchBarInput");
     filter = input.value.toUpperCase();
     list = document.getElementById("exercises_list");
     exercises = list.getElementsByTagName("li");
     for (i = 0; i < exercises.length; i++) {
-        h1 = exercises[i].getElementsByTagName("h1")[0];
-        if (h1.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        p = exercises[i].getElementsByTagName("p")[0];
+        if (p.innerHTML.toUpperCase().indexOf(filter) > -1) {
             exercises[i].style.display = "";
         } else {
             exercises[i].style.display = "none";
 
         }
     }
+
+
 }
+function insertDay() {
+    var table_days = document.getElementById("table_days");
+    var newDay = document.createElement('td');
+    var elementid = document.getElementsByTagName("td").length;
+    newDay.setAttribute('id',elementid);
+    newDay.innerHTML = "<p>"+elementid+"</p>";
+    table_days.appendChild(newDay);
+}
+
