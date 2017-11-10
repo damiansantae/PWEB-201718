702663284fc8610bebbb6289104aa6ed93670bca
Damian <damiansantae@gmail.com>
2017-11-10 19:28:13 +0000

    funcion insertRoutine implementada

--- MainFrame/gym_trainer/gym_trainer.js
+++ MainFrame/gym_trainer/gym_trainer.js
@@ -25,20 +25,37 @@ function insertDay(id) {
 
     var newDay = document.createElement('td');
     var table = table_parent.getElementsByTagName('tr').item(0);
-
     newDay.setAttribute('id','routine_'+routineNumber+'_'+nDays);
     newDay.innerHTML = "<p>"+nDays+"</p>";
     table.appendChild(newDay);
 }
 
 function insertRoutine() {
-    var divParent = document.getElementById("routines_nav");
-    var newRoutine = document.createElement('div');
-    var routineId = "routine_"+document.getElementsByClassName("routine_row").length;
+  var divParent = document.getElementById('routines_nav');
+  var nRoutines = divParent.getElementsByClassName('routine_row').length;
+  var newRoutine = document.createElement('div');
+  newRoutine.setAttribute('id','routine_'+nRoutines);
+  newRoutine.setAttribute('class','routine_row');
+  var routineName = nRoutines+1;
 
-    newRoutine.setAttribute('id',routineId);
-    newRoutine.setAttribute('class', "routine_row");
-    newRoutine.innerHTML = "hola";
+  newRoutine.innerHTML = "<div class=\"routine_master hd-12\" >\n" +
+      "                    <img src=\"../images/press_banca.JPG\" class=\"hd-2\">\n" +
+      "                    <p>Routina "+routineName+"</p>\n" +
+      "                </div>\n" +
+      "                <div class=\"routine_detail hd-12\">\n" +
+      "                    <table>\n" +
+      "                        <tr class=\"table_days\">\n" +
+      "                            <td> <input id=\"routine_"+nRoutines+"_btn\" type=\"button\" value=\"add\" onclick=\"insertDay(this.id)\"> </td>\n" +
+      "                            <td id=\"routine_"+nRoutines+"_1\"> 1</td>\n" +
+      "                        </tr>\n" +
+      "                    </table>\n" +
+      "                </div>"
 
-    divParent.appendChild(newRoutine);
+    //Eliminamos boton de añadir para que no aparezca arriba de la nueva routina añadida
+    var btn = document.getElementById("add_routine_btn");
+    btn.parentNode.removeChild(btn);
+
+divParent.appendChild(newRoutine);      //Añadimos la routina
+    divParent.appendChild(btn);         //Añadimos el boton
+
 }
