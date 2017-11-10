13df3312819ace0915397c8d0bfa820fa15d178e
Damian <damiansantae@gmail.com>
2017-11-10 13:27:32 +0000

    imagenes de ejercicios añadidas refactorizando función insertDay

--- MainFrame/gym_trainer/gym_trainer.js
+++ MainFrame/gym_trainer/gym_trainer.js
@@ -18,12 +18,26 @@ function searchExercise() {
 
 
 }
-function insertDay() {
-    var table_days = document.getElementById("table_days");
+function insertDay(id) {
+    var routineNumber = id.substring(8, 9);
+    var table_parent = document.getElementById('routine_'+routineNumber);
+    var nDays = table_parent.getElementsByTagName('td').length;
+
     var newDay = document.createElement('td');
-    var elementid = document.getElementsByTagName("td").length;
-    newDay.setAttribute('id',elementid);
-    newDay.innerHTML = "<p>"+elementid+"</p>";
-    table_days.appendChild(newDay);
+    var table = table_parent.getElementsByTagName('tr');
+
+    newDay.setAttribute('id','routine_'+routineNumber+'_'+nDays);
+    newDay.innerHTML = "<p>"+nDays+"</p>";
+    table.appendChild(newDay);
 }
 
+function insertRoutine() {
+    var divParent = document.getElementById("routines_nav");
+    var newRoutine = document.createElement('div');
+    var routineId = "routine_"+document.getElementsByClassName("routine_row").length;
+    newRoutine.setAttribute('id',routineId);
+    newRoutine.setAttribute('class', "routine_row");
+    newRoutine.innerHTML = "hola";
+
+    divParent.appendChild(newRoutine);
+}
