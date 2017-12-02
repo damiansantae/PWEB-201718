20c94fe5cdb1504beedbd6bccd319a802b9b11e1
MartinVazquezOtero <martin.vazquez101@alu.ulpgc.es>
2017-12-02 16:57:18 +0000

    validacion user y registro

--- MainFrame/js/mainFrame.js
+++ MainFrame/js/mainFrame.js
@@ -27,7 +27,10 @@ function openTab(evt, tabName) {
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    if (!!evt) {
+        evt.currentTarget.className += " active";
+    }
+
 }
 
 function checkLoginState() {
